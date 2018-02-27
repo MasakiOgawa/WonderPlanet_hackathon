aecebc1a4f9e205e83e546efa533ce2c3cca821f
将輝 小川 <jrp6exu3f@outlook.jp>
2018-02-27 19:43:42 +0900

    新規プロジェクトを追加

---
+++ カーリングファイト/Assets/Marvest/Scripts/Sounds/SoundEffect.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Marvest.Sounds
+{
+    public class SoundEffect : MonoBehaviour
+    {
+        public AudioClip SeHit;
+        public AudioClip SeBulletHit;
+
+        private AudioSource audioSource;
+
+        private void Start()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        public void PlayHitSe()
+        {
+            audioSource.PlayOneShot(SeHit);
+        }
+
+        public void PlayBulletHitSe()
+        {
+            audioSource.PlayOneShot(SeBulletHit);
+        }
+    }
+}
